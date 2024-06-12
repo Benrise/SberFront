b2464db1fe19ae08634da4547061bed474eb7954
Benrise <benrise_x@mail.ru>
2024-06-12 18:17:36 +0300

    Initial commit

---
+++ src/app/app.tsx
@@ -0,0 +1,8 @@
+import { Button } from '@/shared/ui/button'
+import { FunctionComponent } from 'react'
+
+export const App: FunctionComponent = () => (
+  <div>
+    <Button>Button</Button>
+  </div>
+)
